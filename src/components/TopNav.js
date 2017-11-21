b2eba81fbf08455c32b0cb4cc36659dcd12fc45d
jessica <jessica.mcinchak@gmail.com>
2017-11-21 09:47:07 -0500

    nicer nav styling

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TopNav = () => (
-  <div>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-    </ul>
-  </div>
+  <nav className="pa3 pa4-ns">
+    <a className="link dim black b f6 f5-ns dib mr3"><Link to="/">Home</Link></a>
+    <a className="link dim gray f6 f5-ns dib mr3"><Link to="/about">About</Link></a>
+  </nav>
 )
 
 export default TopNav;

db9bd0a388d899216375407fe0b3f948fb49d364
jessica <jessica.mcinchak@gmail.com>
2017-11-22 15:50:33 -0500

    all the metadata

--- src/data/routes.js
+++ src/data/routes.js
@@ -1,72 +1,138 @@
-const ddotRoutes = [
-  {
-    routeId: 'DDOT_',
-    short: '007',
+const ddotRoutes = {
+  7: {
     description: 'Route 7-Cadillac-Harper runs 7 days a week between Mack & Moross and Rosa Parks Transit Center.',
     transferlessTransfer: 'On Saturdays from 7AM-8PM, this bus departs Rosa Parks Transit Center after a layover as Route 10-Chene. Riders remaining on the bus through the layover may continue without using a transfer.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '009',
+  9: {
     description: 'Route 9-Chalmers runs 7 days a week between Jefferson & Maryland and Waltham & 8 Mile.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '010',
+  10: {
     description: 'Route 10-Chene runs 7 days a week between Varjo & Van Dyke and Rosa Parks Transit Center.',
     transferlessTransfer: 'On Saturdays, this bus departs Rosa Parks Transit Center after a layover as Route 7-Cadillac-Harper. Riders remaining on the bus through the layover may continue without using a transfer.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '011',
+  11: {
     description: 'Route 11-Clairmount runs Monday-Friday between Clark & Fort and Lycaste & Jefferson.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '012',
+  12: {
     description: 'Route 12-Conant runs 7 days a week between the State Fairgrounds and Belle Isle.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '013',
+  13: {
     description: 'Route 13-Conner runs 7 days a week between Conner & Jefferson and the Bel Air Center.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '014',
+  14: {
     description: 'Route 14-Crosstown runs Monday-Friday between Warren & Telegraph and Moross & Mack.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '015',
-    description: 'Route 15-Chicago-Davison runs 7 days a week between Orangelawn & Burt and 6 Mile & Joseph Campau.'
+  15: {
+    description: 'Route 15-Chicago-Davison runs 7 days a week between Orangelawn & Burt and 6 Mile & Joseph Campau.' 
   },
-  {
-    routeId: 'DDOT_',
-    short: '016',
+  16: {
     description: 'Route 16-Dexter runs 7 days a week between Jefferson & Shelby and JL Hudson Drive & Greenfield.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '017',
+  17: {
     description: 'Route 17-Eight Mile runs 7 days a week between 7 Mile & Grandriver and Moross & Mack.'
   },
-  {
-    routeId: 'DDOT_', 
-    short: '018',
+  18: {
     description: 'Route 18-Fenkell runs 7 days a week between Fenkell & Dale and Rosa Parks Transit Center.'
   },
-  {
-    routeId:'DDOT_6617',
-    short: '019',
+  19: {
     description: 'Route 19-Fort runs 7 days a week between Fort & W Outer Drive and Rosa Parks Transit Center.',
     transferlessTransfer: 'On Sundays, this bus departs Rosa Parks Transit Center after a layover as Route 49-Vernor. Riders remaining on the bus through the layover may continue without using a transfer.'
   },
-  {
-    routeId: 'DDOT_',
-    short: '021',
+  21: {
     description: 'Route 21-Grand River runs 7 days a week between Grand River & 7 Mile and Rosa Parks Transit Center.'
+  },
+  22: {
+    description: 'Route 22-Greenfield runs 7 days a week between Providence Hospital and Fairlane Mall.'
+  },
+  23: {
+    description: 'Route 23-Hamilton John R runs 7 days a week between Meijer Gateway at 8 Mile and Rosa Parks Transit Center.'
+  },
+  25: {
+    description: 'Route 25-Jefferson runs 7 days a week between Jefferson & Maryland and Rosa Parks Transit Center.',
+    transferlessTransfer: '...' 
+  },
+  27: {
+    description: 'Route 27-Joy runs 7 days a week between Redford Plaza and Rosa Parks Transit Center.',
+    transferlessTransfer: 'On Saturdays, Sundays and weekday nights, this bus departs Rosa Parks Transit Center after a layover as Route 29-Linwood. Riders remaining on the bus through the layover may continue without using a transfer.'
+  },
+  29: {
+    description: 'Route 29-Linwood runs 7 days a week between Livernois & Florence and Rosa Parks Transit Center.',
+    transferlessTransfer: 'On Saturdays, Sundays and weekday nights, this bus departs Rosa Parks Transit Center after a layover as Route 27-Joy. Riders remaining on the bus through the layover may continue without using a transfer.'
+  },
+  30: {
+    description: 'Route 30-Livernois runs 7 days a week between the State Fairgrounds and Jefferson & Brennan.'
+  },
+  31: {
+    description: 'Route 31-Mack runs 7 days a week between Mack & Moross and Rosa Parks Transit Center.'
+  },
+  32: {
+    description: 'Route 32-McNichols runs 7 days a week between Meijer at Old Redford and Cadieux & Mack.'
+  },
+  34: {
+    description: 'Route 34-Gratiot runs 7 days a week between Gratiot & 8 Mile and Third & Michigan.'
+  },
+  37: {
+    description: 'Route 37-Michigan runs 7 days a week between Fairlane Mall and Rosa Parks Transit Center.'
+  },
+  38: {
+    description: 'Route 38-Plymouth runs 7 days a week between Hayes & 8 Mile and Middlebelt & Schoolcraft. At night and on Saturdays, Route 38-Plymouth runs as Route 8-Caniff.'
+  },
+  39: {
+    description: 'Route 39-Puritan runs 7 days a week between Southfield & Fenkell and Manchester & Woodward.'
+  },
+  40: {
+    description: 'Route 40-Russell runs Monday-Friday between E Outer Dr & Van Dyke and Rosa Parks Transit Center.'
+  },
+  41: {
+    description: 'Route 41-Schaefer runs 7 days a week between Brennan & W Jefferson and Schaefer & 8 Mile.'
+  },
+  42: {
+    description: 'Route 42-Mid-City Loop runs 7 days a week. It starts and ends at Manchester & Woodward, traveling clockwise to Oakland, Canfield and Rosa Parks.'
+  },
+  43: {
+    description: 'Route 43-Schoolcraft runs 7 days a week between Redford Plaza and 8 Mile & Ryan.'
+  },
+  45: {
+    description: 'Route 45-Seven Mile runs 7 days a week between Meijer at Old Redford and Moross & Mack.'
+  },
+  46: {
+    description: 'Route 46-Southfield runs Monday-Friday between Fairlane Center and Rutland & 9 Mile.'
+  },
+  47: {
+    description: 'Route 47-Tireman runs Monday-Saturday between Pierson Loop and John R & Mack.'
+  },
+  48: {
+    description: 'Route 48-Van Dyke-Lafayette runs 7 days a week between the Bel Air Center and Rosa Parks Transit Center.',
+    transferlessTransfer: 'On weekday and Saturday nights, this bus departs Rosa Parks Transit Center after a layover as Route 25-Jefferson. Riders remaining on the bus through the layover may continue without using a transfer.'
+  } 
+  49: {
+    description: 'Route 49-Vernor runs 7 days a week between Michigan & Schaefer and Rosa Parks Transit Center.',
+    transferlessTransfer: 'On Sundays, this bus departs Rosa Parks Transit Center after a layover as Route 19-Fort. Riders remaining on the bus through the layover may continue without using a transfer.'
+  },
+  53: {
+    description: 'Route 53-Woodward runs 7 days a week between State Fairgrounds and Rosa Parks Transit Center.'
+  },
+  54: {
+    description: 'Route 54-Wyoming runs 7 days a week between State Fairgrounds and W Jefferson & West End.'
+  },
+  60: {
+    description: 'Route 60-Evergreen runs 7 days a week between Fairlane Center and NW Highway & 10 Mile.'
+  },
+  80: {
+    description: 'Route 80-Villages Direct runs Monday-Friday between W Grand Blvd & Lodge and Maryland & Jefferson.'
+  },
+  89: {
+    description: 'Route 89-Southwest Direct runs Monday-Friday between W Grand Blvd & Trumbull and Fort & Outer Dr.'
+  },
+  92: {
+    description: 'Route 92-Rosedale Express runs Monday-Friday between Larned & St Antoine and Evergreen & Pembroke.'
+  },
+  95: {
+    description: 'Route 95-Ryan Express runs Monday-Friday between E Outer Dr & Van Dyke and Congress & Washington.'
+  },
+  96: {
+    description: 'Route 96-Joy Express runs Monday-Friday between Jefferson & St Antoine and Evergreen & Capitol.'
   }
-];
+};
 
 export default ddotRoutes;

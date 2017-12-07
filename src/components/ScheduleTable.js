5d735fdc8808a3c48798dfd88c9506e53d4fdffd
jessica <jessica.mcinchak@gmail.com>
2017-12-07 11:46:28 -0500

    add scripts to fix router bug for gh-pages

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -2,19 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ScheduleTable = ({schedule, direction}) => (
-  <div class="h4 ma4">
-    <table class="h3 overflow-auto">
-      <tbody class="">
+  <div className="h4 ma4">
+    <table className="h3 overflow-auto">
+      <tbody className="">
       <tr>
         {schedule[direction].stops.map(s => (
-          <th class="pv1 ph2 rotate f6 bg-moon-gray fw5" key={s}>{s}</th>
+          <th className="pv1 ph2 rotate f6 bg-moon-gray fw5" key={s}>{s}</th>
         ))}
       </tr>
 
       {schedule[direction].trips.map(t => (
-        <tr class="striped--light-gray" key={t.trip_id}>
+        <tr className="striped--light-gray" key={t.trip_id}>
           {t.timepoints.map(tp => (
-            <td class="pa1 w7rem tc f6 fw4" key={tp}>{tp}</td>
+            <td className="pa1 w7rem tc f6 fw4" key={tp}>{tp}</td>
           ))}
         </tr>
       ))}

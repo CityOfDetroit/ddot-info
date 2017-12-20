c7e126db95059646321e5467696c908c81fe4f47
Jimmy McBroom <james.mcbroom@gmail.com>
2017-12-20 18:14:21 -0500

    try vertical ScheduleTable

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -5,17 +5,13 @@ import _ from 'lodash';
 export default class ScheduleTable extends Component {
   render() {
     return (
-      <div className="" style={{height: '500px'}}>
-        <StickyTable className="" stickyColumnCount={0}>
-          <Row className="fw7">
-            {this.props.schedule[this.props.direction].stops.map((s, i) => (
-              <Cell className="pa2 fw6 f7 tc v-btm bg-gray white w4" key={i}>{_.toUpper(s)}</Cell>
-            ))}
-          </Row>
-          {this.props.schedule[this.props.direction].trips.map((t, j) => (
-            <Row className="striped--near-white" key={j}>
-              {t.timepoints.map((tp, k) => (
-                <Cell className="pa1 tc f6 fw4 w6 min-w3 v-btm" key={k}>{tp}</Cell>
+      <div className="" style={{width: '600px'}}>
+        <StickyTable className="" stickyRowCount={0}>
+          {this.props.schedule[this.props.direction].stops.map((s, i) => (
+            <Row key={i}>
+              <Cell className="bg-dark-gray white fw7 f7 pa2 w5" style={{'min-width': '200px'}}>{s}</Cell>
+              {this.props.schedule[this.props.direction].trips.map((t) => (
+                <Cell className="pa1 striped--near-white">{t.timepoints[i]}</Cell>
               ))}
             </Row>
           ))}

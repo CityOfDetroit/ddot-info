78b56133b6cf8495a3fc2ec6b530e92fc31a2900
Jimmy McBroom <james.mcbroom@gmail.com>
2018-01-04 22:18:19 -0500

    no useless constructor warning

--- src/components/RealtimeTripList.js
+++ src/components/RealtimeTripList.js
@@ -2,22 +2,14 @@ import React, { Component } from 'react';
 import RealtimeTrip from './RealtimeTrip'
 
 export default class RealtimeTripList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-      console.log(this.props)
-      return (
-        <div className="">
-          currently running: {this.props.trips.length} buses
-          {this.props.trips.map(t =>
-            <RealtimeTrip trip={t} />
-          )}
-        </div>
-      );
-    // }
-
-
+    return (
+      <div className="">
+        currently running: {this.props.trips.length} buses
+        {this.props.trips.map(t =>
+          <RealtimeTrip trip={t} />
+        )}
+      </div>
+    );
   }
 }

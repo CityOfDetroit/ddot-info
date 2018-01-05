f5497ce619c9790978c5121f4d10efacf2af126d
Jimmy McBroom <james.mcbroom@gmail.com>
2018-01-05 12:53:35 -0500

    direction colors is a config js file

---
+++ src/data/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+  'northbound': 'rgb(58, 67, 201)',
+  'eastbound': 'rgb(58, 67, 201)',
+  'southbound': '#ba25b0',
+  'westbound': '#ba25b0',
+  'clockwise': 'rgb(23, 214, 42)'   
+}
+
+export default Colors;

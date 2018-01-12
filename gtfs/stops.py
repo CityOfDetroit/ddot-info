4e62d1323d6033d80d239fc66e6ce9ddb4596461
James McBroom <McBroomJ@ds.detroitmi.gov>
2018-01-12 14:09:25 -0500

    saint amy

--- gtfs/stops.py
+++ gtfs/stops.py
@@ -1,10 +1,10 @@
 import sqlalchemy
 import json
-engine = sqlalchemy.create_engine('postgresql://localhost/iet')
+engine = sqlalchemy.create_engine('postgresql://jimmy@localhost/iet')
 conn = engine.connect()
 
 if __name__ == "__main__":
-  query = "select stop_id, stop_name from gtfs.stops"
+  query = "select stop_id, stop_name, stop_lat, stop_lon from gtfs.stops"
   res = conn.execute(query)
   stops_object = { int(row[0]): row[1] for row in res.fetchall() }
   with open("stops.json", 'w') as f:

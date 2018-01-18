2516f95afa755d2618889b5d0e3f4f2c4f96b952
Jimmy McBroom <james.mcbroom@gmail.com>
2018-01-18 18:04:02 -0500

    add routes to stop.js

--- gtfs/stops.py
+++ gtfs/stops.py
@@ -4,8 +4,19 @@ engine = sqlalchemy.create_engine('postgresql://jimmy@localhost/iet')
 conn = engine.connect()
 
 if __name__ == "__main__":
-  query = "select stop_id, stop_name, stop_lat, stop_lon from gtfs.stops"
+  query = """
+      select 
+      sp.stop_id, 
+      sp.stop_name, 
+      sp.stop_lat, 
+      sp.stop_lon, 
+      json_agg(distinct regexp_replace(rt.route_short_name, '^0{1,}', ''))
+    from gtfs.stops sp
+    inner join gtfs.stop_times st on sp.stop_id = st.stop_id
+    inner join gtfs.trips tr on tr.trip_id = st.trip_id
+    inner join gtfs.routes rt on tr.route_id = rt.route_id
+    group by sp.stop_id"""
   res = conn.execute(query)
-  stops_object = { int(row[0]): {'name': row[1], 'lat': row[2], 'lon': row[3]} for row in res.fetchall() }
+  stops_object = { int(row[0]): {'name': row[1], 'lat': row[2], 'lon': row[3], 'routes': row[4]} for row in res.fetchall() }
   with open("stops.json", 'w') as f:
     f.write(json.dumps(stops_object))

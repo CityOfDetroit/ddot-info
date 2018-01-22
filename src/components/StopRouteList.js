e85b62be5a7d8c9753dee7cec78b52373ab756fd
James McBroom <McBroomJ@ds.detroitmi.gov>
2018-01-22 16:04:35 -0500

    don't show buses arriving in the 1960s

--- src/components/StopRouteList.js
+++ src/components/StopRouteList.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import Schedules from '../data/schedules.js';
 import LineLink from './LineLink'
 import _ from 'lodash';
+import moment from 'moment';
 
 import RoutePredictionList from './RoutePredictionList';
 
 class StopRouteList extends Component {
   render () {
     const predictions = this.props.predictions
+    const goodPredictions = _.filter(this.props.predictions.data.entry.arrivalsAndDepartures, a => { return a.predictedArrivalTime > 0 })
     const routes = this.props.routes
     return (
       <div className='list overflow-scroll'>
         {routes.map(r => (
           <div className="pa2 bb" style={{display: 'flex', alignItems: 'center'}}>
             <LineLink name={Schedules[r].rt_name} id={r} color={Schedules[r].color} />
-            <RoutePredictionList predictions={_.filter(predictions.data.entry.arrivalsAndDepartures, function(o) { return o.routeShortName === r.padStart(3, '0')})} route={r} />
+            <RoutePredictionList predictions={_.filter(goodPredictions, function(o) { return o.routeShortName === r.padStart(3, '0')})} route={r} />
           </div>
         ))}
       </div>

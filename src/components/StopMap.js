85308d857d0064ad9c47443799e4807aaff29ae3
Jimmy McBroom <james.mcbroom@gmail.com>
2018-02-08 14:53:42 -0500

    make ze map move

--- src/components/StopMap.js
+++ src/components/StopMap.js
@@ -40,17 +40,27 @@ class StopMap extends Component {
       map.setLayoutProperty('ddot-stops-label-highlight', 'visibility', 'visible')
     });
 
-
-
     this.setState({
       drewMap: true,
       map: map,
     });
   }
 
-
   componentDidMount() {
     this.drawMap()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props.stopId !== nextProps.stopId) {
+
+      const transferIds = Stops[nextProps.stopId].transfers.map(t => t[0])
+      this.state.map.setFilter('ddot-stops', ["in", "stop_id"].concat(transferIds))
+      this.state.map.setFilter('ddot-stops copy', ["in", "stop_id"].concat(transferIds))
+      this.state.map.setFilter('ddot-stops-highlight', ["in", "stop_id", nextProps.stopId])
+      this.state.map.setFilter('ddot-stops-label-highlight', ["in", "stop_id", nextProps.stopId])
+
+
+    }
   }
 
   render() {

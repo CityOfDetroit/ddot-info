cf129a5326ae7b31d64c6072ba24139eedd4c9de
jessica <jessica.mcinchak@gmail.com>
2018-02-27 10:11:38 -0500

    add about icon to all headers, comment out live tab for routes

--- src/components/RouteHeader.js
+++ src/components/RouteHeader.js
@@ -4,36 +4,42 @@ import PropTypes from 'prop-types';
 
 import Schedules from '../data/schedules.js';
 import ChevronSVG from '../img/chevron.svg';
+import Info from '../img/info_outline.svg';
 
 const RouteHeader = ({ number }) => {
   const thisRoute = Schedules[number];
   const color = thisRoute.color;
   const name = thisRoute.rt_name;
-  const page = window.location.pathname.split('/').slice(-1)[0]
+  const page = window.location.pathname.split('/').slice(-1)[0];
 
   return (
-    <div className="nav header" style={{background: '#004445'}}>
-      <div className="pa3" style={{display: 'flex', alignItems: 'center', }}>
-        <Link className="link dim pr3" to={{ pathname: `/` }}>
-          <img src={ChevronSVG} className="" alt="Go back" />
-        </Link>
+    <div className="nav header" style={{ background: '#004445' }}>
+      <div className="pa3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
         <div>
-          <div style={{ display: 'flex', alignItems:'center', justifyContent: 'flex-start'}}>
-          <Link className="link dim" to={{ pathname: `/route/${number}`}} > 
-            <div className='white fw7 f3 tc' style={{ display: 'flex', alignItems:'center', justifyContent: 'center', width: '2em', height: '2em', backgroundColor: color }}>
-              {number}
-            </div> 
-          </Link>
-            <div className='white glow ph3' style={{fontSize: `2em`}}>
+          <div style={{ display: 'flex', alignItems:'center', justifyContent: 'flex-start' }}>
+            <Link className="link dim pr3" to={{ pathname: `/` }}>
+              <img src={ChevronSVG} className="" alt="Go back" />
+            </Link>
+            <Link className="link dim" to={{ pathname: `/route/${number}`}} > 
+              <div className='white fw7 f3 tc' style={{ display: 'flex', alignItems:'center', justifyContent: 'center', width: '2em', height: '2em', backgroundColor: color }}>
+                {number}
+              </div> 
+            </Link>
+            <div className='white glow ph3' style={{ fontSize: `2em` }}>
               {name}
             </div>
           </div>
         </div>
+        <div>
+          <Link className="link dim pr3" to={{ pathname: `/about` }}>
+            <img src={Info} alt="Info" />
+          </Link>
+        </div>
       </div>
       <div style={{ display: 'flex' }}>
-         <Link className="link dim white pv2 ph3 ml3" style={page === 'real-time' ? {background: `white`, color: `black`} : {background: 'rgba(255,255,255,0.2)'}} to={{ pathname: `/route/${number}/real-time` }}>
+         {/* <Link className="link dim white pv2 ph3 ml3" style={page === 'real-time' ? {background: `white`, color: `black`} : {background: 'rgba(255,255,255,0.2)'}} to={{ pathname: `/route/${number}/real-time` }}>
            Live
-         </Link>
+         </Link> */}
          <Link className="link dim white pv2 ph3 ml3" style={page === 'stops' ? {background: `white`, color: `black`} : {background: 'rgba(255,255,255,0.2)'}} to={{ pathname: `/route/${number}/stops` }}>
            Stops
          </Link>
@@ -42,7 +48,7 @@ const RouteHeader = ({ number }) => {
          </Link>
       </div>
     </div>
-  )
+  );
 }
 
 RouteHeader.propTypes = {
--- src/components/StopHeader.js
+++ src/components/StopHeader.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import ChevronSVG from '../img/chevron.svg';
+import Info from '../img/info_outline.svg';
 
 const StopHeader = ({ id, name }) => (
-
-    <div className="nav header" style={{background: '#004445'}}>
-    <div className="pa3" style={{display: 'flex', alignItems: 'center'}}>
-      <Link className="link dim pr3" to={{ pathname: `/` }}>
-        <img src={ChevronSVG} className="" alt="Weird bus logo" />
-      </Link>
-      <span className="f3 pa2 fw7 mr2" style={{background: 'hsl(60, 84%, 49%)'}}>
-        #{id}
-      </span>
-      <span className="f3 mh2 pa2 fw5 white">
-        {name}
-      </span>
+  <div className="nav header" style={{ background: '#004445' }}>
+    <div className="pa3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <div>
+        <Link className="link dim pr3" to={{ pathname: `/` }}>
+          <img src={ChevronSVG} className="" alt="Weird bus logo" />
+        </Link>
+        <span className="f3 pa2 fw7 mr2" style={{background: 'hsl(60, 84%, 49%)'}}>
+          #{id}
+        </span>
+        <span className="f3 mh2 pa2 fw5 white">
+          {name}
+        </span>
+      </div>
+      <div>
+        <Link className="link dim pr3" to={{ pathname: `/about` }}>
+          <img src={Info} alt="Info" />
+        </Link>
+      </div>
     </div>
   </div>
 );

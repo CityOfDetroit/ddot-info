37d8ae2700d5beab742c7630d7ab028cb7295e08
Jimmy McBroom <james.mcbroom@gmail.com>
2018-02-27 22:25:01 -0500

    cleanup & fix 1st column bug

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -15,11 +15,10 @@ class ScheduleTable extends Component {
 
     return (
       <div className="v-top schedule">
-        <StickyTable className="h6 v-top">
+        <StickyTable className="h6 v-top" stickyColumnCount={0}>
           <Row className="fw7">
-          {/* <Cell className="pa2 fw6 f7 tc v-top black bb br tripid w2"></Cell> */}
             {this.props.schedule[this.props.direction].stops.map((s, i) => (
-              <Cell className="pa2 fw6 f7 tc v-top black bb w2 sched" style={{backgroundColor: background}} key={i}>
+              <Cell className="pa2 fw6 f7 tc v-top black bb w2 sched" style={{backgroundColor: background}} key={s}>
                 <Link to={{pathname: `/stop/${s}/`}} >
                   {Stops[s].name}
                 </Link>
@@ -32,12 +31,6 @@ class ScheduleTable extends Component {
               key={t.trip_id}
               style={tripsToHighlight.indexOf(t.trip_id) > -1 ? {color: active, fontWeight: 'bold', backgroundColor: background} : {}}
               >
-                {/* <Cell 
-                  className={(j+1) % 5 === 0 ? "pa2 bb br tc f7 fw5 v-btm tripid" : "pa2 br tc f7 fw5 v-btm tripid"}  
-                  style={(j+1) % 5 === 0 ? {borderBottomColor: background, borderBottomWidth: '4px'} : {} }
-                >
-                  {t.trip_id}
-                </Cell>               */}
               {t.timepoints.map((tp, k) => (
                 <Cell 
                   className={(j+1) % 5 === 0 ? "pa2 bb br tc f6 v-mid w5 fw5 sched" : "pa2 br tc f6 v-mid fw5 w5 sched"}  

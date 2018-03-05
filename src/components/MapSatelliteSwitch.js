4aa1109ab686289d1ce87be8dd428d77583d5819
jessica <jessica.mcinchak@gmail.com>
2018-03-05 10:37:21 -0500

    absolute position for satellite toggle

--- src/components/MapSatelliteSwitch.js
+++ src/components/MapSatelliteSwitch.js
@@ -4,7 +4,7 @@ class MapSatelliteSwitch extends React.Component {
 
     render() {
         return (
-            <div className="fixed pa2 bg-white o-80" style={{zIndex: 2}}>
+            <div className="absolute pa2 bg-white o-80" style={{zIndex: 2}}>
                 <label className="mr2" htmlFor="satellite">Aerial imagery
                 </label>
                 <input type="checkbox" id="satellite" value="satellite"  onChange={this.props.onChange} />

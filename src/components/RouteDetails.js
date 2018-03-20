a8aa8c35688544a73b74729371d9e81fe9b2afa9
jessica <jessica.mcinchak@gmail.com>
2018-03-20 12:21:22 -0400

    cards in cards in cards

--- src/components/RouteDetails.js
+++ src/components/RouteDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import { Card, CardText, CardTitle } from 'material-ui/Card';
+import { Card, CardText, CardTitle, CardHeader } from 'material-ui/Card';
 
 import routeDetails from '../data/routeDetails.js';
 import PrintSchedule from './PrintSchedule';
@@ -15,37 +15,33 @@ const RouteDetails = ({ id }) => {
           {obj.description}
         </CardText>
       </Card>
-      <PrintSchedule routePdf={obj.pdf} />
       <Card className="mv2">
-        <CardTitle title="Service summary:" />
+        <CardTitle title="Service overview" />
         {["Monday-Friday", "Saturday", "Sunday/Holiday"].map((d, i) => (
-          <div className="pa1 w-100 mb3" key={i}>
-            <div className="f4 w-100">
-              <span className="dib fw7 pb1">{d}</span>
-            </div>
+          <div>
             {obj.services[d] ?
-              (<div>
-                <div className="f5">
-                <span className="fw5">
-                  {obj.services[d].service_hours.length === 1 ? obj.services[d].service_hours[0] : `${obj.services[d].service_hours[0]} - ${obj.services[d].service_hours[1]}` }</span>
-                </div>
-                <div className="mt2">
+              <Card key={i}>
+                <CardHeader
+                  title={d}
+                  subtitle={obj.services[d].service_hours.length === 1 ? obj.services[d].service_hours[0] : `${obj.services[d].service_hours[0]} - ${obj.services[d].service_hours[1]}`} />
+                <CardText>
                   <table className="w-100">
                     <tbody>
                       {obj.services[d].frequency.map((f, i) => (
                         <tr key={i}>
-                          <td className='w-50 bg-moon-gray fw7 pa2'>{f[0]}</td>
-                          <td className='w-50 bg-moon-gray pa2'>every <span className='fw7'>{f[1]}</span> minutes</td>
+                          <td className='w-50 bg-light-gray fw7 pa2'>{f[0]}</td>
+                          <td className='w-50 bg-light-gray pa2'>every <span className='fw7'>{f[1]}</span> minutes</td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
-                </div>
-              </div> ) 
-            : `No service.`}
+                </CardText>
+              </Card> 
+            : '' }
           </div>
         ))}
       </Card>
+      <PrintSchedule routePdf={obj.pdf} />
     </div>
   );
 }

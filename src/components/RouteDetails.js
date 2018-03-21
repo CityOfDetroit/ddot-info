df71453d4f35903b45e6968cca9e32c9ec88cdad
jessica <jessica.mcinchak@gmail.com>
2018-03-21 10:15:34 -0400

    lets try schedule table in a card instead of a plain div

--- src/components/RouteDetails.js
+++ src/components/RouteDetails.js
@@ -37,7 +37,7 @@ const RouteDetails = ({ id }) => {
                   </table>
                 </CardContent>
               </Card> 
-            : <Card key={i}><CardHeader title={d} subtitle="No service" /></Card> }
+            : <Card key={i}><CardHeader title={d} subheader="No service" /></Card> }
           </div>
         ))}
       </Card>
--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { StickyTable, Row, Cell } from 'react-sticky-table';
 import { Link } from 'react-router-dom';
 import chroma from 'chroma-js';
+import Card, { CardContent } from 'material-ui/Card';
 
 import Stops from '../data/stops.js';
 
@@ -13,37 +14,39 @@ class ScheduleTable extends Component {
     let tripsToHighlight = this.props.liveTrips.map(t => t.slice(8));
 
     return (
-      <div className="v-top">
-        <StickyTable className="h6 v-top" stickyColumnCount={0}>
-          <Row>
-            {this.props.schedule[this.props.direction].stops.map((s, i) => (
-              <Cell 
-                className="pa2 fw5 f6 tc v-btm black bb w2 sched" 
-                style={{ borderBottomColor: this.props.color, borderBottomWidth: '10px' }} 
-                key={s}>
-                <Link className="dim black" to={{ pathname: `/stop/${s}/` }} >
-                  {Stops[s].name.indexOf('Rosa Parks') > -1 ? "Rosa Parks TC" : Stops[s].name}
-                </Link>
-              </Cell>
-            ))}
-          </Row>
-          {this.props.schedule[this.props.direction].trips.map((t, j) => (
-            <Row 
-              className="striped--near-white" 
-              key={t.trip_id}
-              style={tripsToHighlight.indexOf(t.trip_id) > -1 ? { color: active, fontWeight: 'bold', backgroundColor: background } : {}}>
-              {t.timepoints.map((tp, k) => (
+      <Card>
+        <CardContent>
+          <StickyTable className="h6 v-top" stickyColumnCount={0}>
+            <Row>
+              {this.props.schedule[this.props.direction].stops.map((s, i) => (
                 <Cell 
-                  className={(j+1) % 5 === 0 ? "pa2 bb br tc f6 v-mid w5 fw5 sched" : "pa2 br tc f6 v-mid fw5 w5 sched"}  
-                  style={(j+1) % 5 === 0 ? { borderBottomColor: this.props.color, borderBottomWidth: '4px' } : {} }
-                  key={k}>
-                  {tp}
+                  className="pa2 fw5 f6 tc v-btm black bb w2 sched" 
+                  style={{ borderBottomColor: this.props.color, borderBottomWidth: '10px' }} 
+                  key={s}>
+                  <Link className="dim black" to={{ pathname: `/stop/${s}/` }} >
+                    {Stops[s].name.indexOf('Rosa Parks') > -1 ? "Rosa Parks TC" : Stops[s].name}
+                  </Link>
                 </Cell>
               ))}
             </Row>
-          ))}
-        </StickyTable>
-      </div>
+            {this.props.schedule[this.props.direction].trips.map((t, j) => (
+              <Row 
+                className="striped--near-white" 
+                key={t.trip_id}
+                style={tripsToHighlight.indexOf(t.trip_id) > -1 ? { color: active, fontWeight: 'bold', backgroundColor: background } : {}}>
+                {t.timepoints.map((tp, k) => (
+                  <Cell 
+                    className={(j+1) % 5 === 0 ? "pa2 bb br tc f6 v-mid w5 fw5 sched" : "pa2 br tc f6 v-mid fw5 w5 sched"}  
+                    style={(j+1) % 5 === 0 ? { borderBottomColor: this.props.color, borderBottomWidth: '4px' } : {} }
+                    key={k}>
+                    {tp}
+                  </Cell>
+                ))}
+              </Row>
+            ))}
+          </StickyTable>
+        </CardContent>
+      </Card>
     );
   }
 }

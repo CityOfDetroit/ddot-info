1410a49fa4b5b0dc70cf57118e4a56836ee03566
jessica <jessica.mcinchak@gmail.com>
2018-04-05 10:57:35 -0400

    buttons look better, no leading text

--- src/components/PrintSchedule.js
+++ src/components/PrintSchedule.js
@@ -5,10 +5,9 @@ import PdfIcon from 'material-ui-icons/PictureAsPdf';
 
 const PrintSchedule = ({ routePdf }) => (
     <div style={{ display: 'flex', alignItems: 'center' }}>
-      <span style={{ marginRight: '.5em' }}>Download schedule:</span>
       {routePdf.map((p, i) => 
-        <Button key={i} variant="raised" size="small" style={{ margin: '.5em', color: '#eee' }}>
-          <a href={p} style={{ textDecoration: 'none' }}>{routePdf.length === 1 ? `All service` :  i === 0 ? `Weekday` : `Saturday-Sunday` }</a>
+        <Button key={i} variant="raised" style={{ margin: '.5em', color: '#eee', textTransform: 'none' }}>
+          <a href={p} style={{ color: '#000', textDecoration: 'none' }}>{routePdf.length === 1 ? `Schedule` :  i === 0 ? `Weekday schedule` : `Weekend schedule`}</a>
           <PdfIcon style={{ color: '#000', marginLeft: '.1em' }} />
         </Button>
       )}

418d78662357b6c0435c773a2dc16da8143db0d0
Jimmy McBroom <james.mcbroom@gmail.com>
2018-10-31 10:02:32 -0400

    fix print button (fixes #83)

--- src/components/PrintSchedule.js
+++ src/components/PrintSchedule.js
@@ -8,14 +8,14 @@ const PrintSchedule = ({ routePdf }) => (
   <div style={{ display: 'flex', alignItems: 'center', fontSize: '.9em' }}>
     <span>Download:</span>
     {routePdf.map((p, i) => 
-      <Button key={i} variant="raised" size="small" style={{ margin: '.5em', color: '#eee', textTransform: 'none' }}>
-        <a href={p} style={{ color: '#000', textDecoration: 'none' }}>
-          {routePdf.length === 1 ? `All service` 
-            :  i === 0 ? `Weekday` 
-              : i === 2 ? `Weekend - Sunday` :  `Weekend` }
-        </a>
-        <PdfIcon style={{ color: '#000', marginLeft: '.1em' }} />
-      </Button>
+      <a href={p} style={{textDecoration: 'none' }}>
+        <Button key={i} variant="raised" size="small" style={{ margin: '.5em', textTransform: 'none' }}>
+            {routePdf.length === 1 ? `All service` 
+              :  i === 0 ? `Weekday` 
+                : i === 2 ? `Weekend - Sunday` :  `Weekend` }
+          <PdfIcon style={{ color: '#000', marginLeft: '.1em' }} />
+        </Button>
+      </a>
     )}
   </div>
 );
